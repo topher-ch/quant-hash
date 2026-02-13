c384d27f1b26f7ee37e6f4cde196400749d7d856
topher-ch <christopher.n.parnell@gmail.com>
2026-02-13 15:09:09 +1100

    Added testing infrastructure

--- cpp/src/permutation.cpp
+++ cpp/src/permutation.cpp
@@ -1,4 +1,8 @@
 #include "permutation.hpp"
+#ifdef ENABLE_DOCTEST
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+#endif
 
 namespace permutation {
 
@@ -13,5 +17,11 @@ void rho(State& s) {}
 void pi(State& s) {}
 void chi(State& s) {}
 void iota(State& s) {}
+
+#ifdef ENABLE_DOCTEST
+TEST_CASE("Simple zero equals zero") {
+    CHECK(0 == 0);
+}
+#endif
 
 } // namespace permutation::internal
