33c005f9c8c1adf8187be7c63a08a219f8ef3d7d
Christopher Parnell <u7292105@anu.edu.au>
2026-06-29 16:42:47 +1000

    Input padding implemented

--- cpp/src/hash.cpp
+++ cpp/src/hash.cpp
@@ -3,6 +3,24 @@
 
 namespace hash {
 
+std::vector<uint32_t> pad(std::span<const uint8_t> input) {
+    std::vector<uint32_t> padded(input.begin(), input.end());
+    padded.push_back(0x01);
+    while (padded.size() % (4*r_lanes) != 0) {
+        padded.push_back(0x00);
+    }
+    padded.back() |= 0x80;
 
+    std::vector<uint32_t> output;
+    for (int i = 0; i < padded.size(); i += 4) {
+        uint32_t block = 0;
+        block |= (uint32_t)padded[i];
+        block |= (uint32_t)padded[i+1] << 8;
+        block |= (uint32_t)padded[i+2] << 16;
+        block |= (uint32_t)padded[i+3] << 24;
+        output.push_back(block);
+    }
+    return output;
+}
     
 } // namespace hash
--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,8 +1,20 @@
 #include "hash.hpp"
 #include "permutation.hpp"
 #include <iostream>
+#include <iomanip>
 
 int main() {
+    std::array<uint8_t, 9> input{};
+    for (int i = 0; i < 9; i++) {
+        input[i] = 0xf0 + i;
+    }
+    std::vector<uint32_t> padded = hash::pad(input);
+    std::cout << "input (padded):\n";
+    for (int i = 0; i < padded.size(); i++) {
+        std::cout << std::hex << std::setfill('0') << std::setw(8) << padded[i] << " ";
+    }
+    std::cout << "\n\n";
+
     std::array<uint32_t, 9> s{};
     for (int i = 0; i < 9; i++) {
         s[i] = 0xfffffff0 + i;
